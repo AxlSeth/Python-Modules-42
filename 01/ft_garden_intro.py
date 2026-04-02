589d807fbcc4c695a6aa639ac082e5ea616e6573
Axel <seramaro@student.42antananarivo.mg>
2026-04-02 18:22:22 +0300

    andeha hody

--- 01/ft_garden_intro.py
+++ 01/ft_garden_intro.py
@@ -1,1 +1,11 @@
+def ft_garden_intro() -> None:
+    print("=== Welcome to My Garden ===")
+    name = print("Plant: Roses")
+    height = print("Height: 25 cm")
+    age = print("Age: 30 days")
+    print("\n=== End of  Program ===")
 
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
