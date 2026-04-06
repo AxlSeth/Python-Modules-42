83408051b3da76f5a655b951e40fcca216c83b95
Axl <axlramaroson25@gmail.com>
2026-04-06 23:16:37 +0300

    Refactor Plant class and growth function
    Fix plant growth logic and update show method.

--- 01/ft_plant_growth.py
+++ 01/ft_plant_growth.py
@@ -4,14 +4,12 @@ class Plant:
         self.height = height
         self.age = age
 
-    def grow(self, height: int) -> None:
-        for i in range(1, 7):
-            self.height += 0.8
-            print(f"{self.height}")
+    def grow(self) -> None:
+        self.height += 0.8
 
-    def age(self, height: int) -> None:
+    def age(self) -> None:
         self.age += 1
-        print(f"{self.age} days")
+        self.grow()
 
     def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
@@ -22,9 +20,10 @@ class Plant:
 def ft_plant_growth() -> None:
     print("=== Garden Plant Growth ===")
     plant1 = Plant("Tulip", 20, 5)
-    plant1.age()
-    plant1.grow()
-    plant.show()
+    for i in range(1, 7)
+        plant1.age()
+        plant1.grow()
+        plant.show()
 
 if __name__ == "__main__":
     ft_plant_growth()
