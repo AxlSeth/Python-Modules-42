8f9e437f4f0fd2a4291c716b7112b824f90b26a8
Axel <seramaro@student.42antananarivo.mg>
2026-04-14 07:07:09 +0300

    fixed logic

--- 01/ft_garden_security.py
+++ 01/ft_garden_security.py
@@ -32,8 +32,9 @@ class Plant:
         self._plant_height = round((self._plant_height + grow_size), 1)
 
     def age(self) -> None:
-        self._plant_age += 1
+        self.set_age(self.get_age() + 1)
 
     def show(self) -> None:
         print(f"{self._plant_name}:", end="")
         print(f"{self._plant_height}cm, {self._plant_age} days old")
+
