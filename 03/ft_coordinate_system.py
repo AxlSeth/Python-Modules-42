b910d547eee283139d36d97056a96d1c0c7d34cb
Axl <axlramaroson25@gmail.com>
2026-04-23 00:28:16 +0300

    tuple creation works

--- 03/ft_coordinate_system.py
+++ 03/ft_coordinate_system.py
@@ -3,24 +3,30 @@ import math
 
 def get_player_pos() -> tuple[float, float, float]:
     while True:
-        str_pos: str = input("Enter new coordinates as floats in format 'x,y,z': ")
-        usr_input = str_pos.split(',')
+        usr_input: str = input("Enter new coordinates as floats in format 'x,y,z': ")
+        usr_xyz: list[str] = usr_input.split(',')
+        if len(usr_xyz) != 3:
+            print("Invalid syntax")
+            continue
         xyz: list[float] = [0, 0, 0]
-
-        for i in range(0, 3):
-            try:
-                xyz[i] = float(usr_input[i])
-            except TypeError as e1:
-                print(f"Error on parameter '{xyz[i]}': {e1}")
-            except ValueError as e2:
-                print(f"")
-        print(f"Got a first tuple: {x , y, z}")
-        return (x, y, z)
+        try:
+            for i in range(0, 3):
+                xyz[i] = float(usr_xyz[i])
+            return (float(xyz[0]), float(xyz[1]), float(xyz[2]))
+        except ValueError as err:
+            print(f"Error on parameter '{usr_xyz[i]}': {err}")
+            continue
+        except TypeError:
+            print("Invalid syntax")
+            continue
 
 
 def main() -> None:
     print("=== Game Coordinates System ===\n")
-    get_player_pos()
+    print("Get a first set of coordinates")
+    xyz: tuple[float, float, float] = get_player_pos()
+    print(f"Got a first tuple: {xyz}")
+    print(f"It includes: X={xyz[0]}, Y={xyz[1]}, Z={xyz[2]}")
 
 
 if __name__ == "__main__":
