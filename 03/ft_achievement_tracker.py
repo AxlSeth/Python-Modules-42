989ec86faa9798e5cdb19b538eb474ffe70d179a
Axl <axlramaroson25@gmail.com>
2026-04-23 10:49:25 +0300

    achievement tracker

---
+++ 03/ft_achievement_tracker.py
@@ -0,0 +1,15 @@
+import random
+
+def gen_player_achievements() -> None:
+    ...
+
+
+def main() -> None:
+    print("=== Achievement Tracker System ===\n")
+
+
+if __name__ == "__main__":
+    set1: set[str] = {'haha', 'hihi', 'hehe'}
+    set2 = list(set1)
+    print(f"{set2}")
+    main()
