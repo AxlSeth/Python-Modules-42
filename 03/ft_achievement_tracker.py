fc9fa3148229d8f02a846f843eb622f4872c3418
Axl <axlramaroson25@gmail.com>
2026-04-23 17:14:59 +0300

    achievement system half working

--- 03/ft_achievement_tracker.py
+++ 03/ft_achievement_tracker.py
@@ -1,15 +1,54 @@
 import random
 
-def gen_player_achievements() -> None:
-    ...
+
+class Player():
+    def __init__(self,
+                 name: str,
+                 achievements: set[str]) -> None:
+        self._name = name
+        self._achievements = achievements
+
+
+    def get_name(self) -> str:
+        return(self._name)
+
+
+    def get_achievements(self) -> set[str]:
+        return(self._achievements)
+
+    def show_achievements(self) -> None:
+        print(f"Player {self._name}: {self._achievements}")
+
+
+def gen_player_achievements() -> set[str]:
+    achievements: list[str] = ['Crafting Genius', 'Strategist', 'World Savior',
+                              'Speed Runner', 'Survivor', 'Master Explorer',
+                              'Treasure Hunter', 'Unstoppable', 'First Steps',
+                              'Collector Supreme', 'Untouchable', 'Sharp Mind',
+                              'Boss Slayer']
+    p_achievements: set[str] = set() 
+    for _ in range(0, random.randint(1, len(achievements))):
+        p_achievements = p_achievements.union({random.choice(achievements)})
+    return(p_achievements)
 
 
 def main() -> None:
     print("=== Achievement Tracker System ===\n")
+    p1 = Player("Alice", gen_player_achievements())
+    p2 = Player("Bob", gen_player_achievements())
+    p3 = Player("Charlie", gen_player_achievements())
+    p4 = Player("Dylan", gen_player_achievements())
+    p1.show_achievements()
+    p2.show_achievements()
+    p3.show_achievements()
+    p4.show_achievements()
+    print("\nAll distinct achievements: ", end="")
+    print(p1.get_achievements().difference(p2.get_achievements(), p3.get_achievements(),
+                                                p4.get_achievements()))
+    print("\nCommon achievements: ", end="")
+    print(p1.get_achievements().intersection(p2.get_achievements(), p3.get_achievements(),
+                                                p4.get_achievements()))
 
 
 if __name__ == "__main__":
-    set1: set[str] = {'haha', 'hihi', 'hehe'}
-    set2 = list(set1)
-    print(f"{set2}")
     main()
