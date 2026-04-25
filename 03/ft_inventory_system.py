93e683a42c0ae646c87e301771049b779dbdfc15
Axl <axlramaroson25@gmail.com>
2026-04-25 17:46:53 +0300

    code works, no norm

--- 03/ft_inventory_system.py
+++ 03/ft_inventory_system.py
@@ -22,7 +22,25 @@ def create_inventory() -> dict[str, int]:
 
 def main() -> None:
     print("=== Inventory System Analysis ===")
-    create_inventory()
+    inventory: dict[str, int] = create_inventory()
+    print(f"Item list: {list(inventory.keys())}")
+    print(f"Total quantity of the {len(inventory)} items:"
+          f" {sum(inventory.values())}")
+    max_min: tuple[int, int] = (max(inventory.values()), min(inventory.values()))
+    max_key: str = ""
+    min_key: str = ""
+    for item in inventory:
+        print(f"Item {item} represents"
+              f" {round(inventory[item] * 100 / sum(inventory.values()), 1)}%")
+        if inventory[item] == max_min[0]:
+            max_key = item
+        elif inventory[item] == max_min[1]:
+            min_key = item
+    print(f"Item most abundant: {max_key} with quantity {inventory[max_key]}")
+    print(f"Item least abundant: {min_key} with quantity {inventory[min_key]}")
+    inventory.update({"magic_item": 1})
+    print(f"Updated inventory: {inventory}")
+
 
 if __name__ == "__main__":
     main()
