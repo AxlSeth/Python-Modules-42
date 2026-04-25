a4bd5307870a0c8923db8eef5fb7b927f80de9f4
Axl <axlramaroson25@gmail.com>
2026-04-25 12:20:02 +0300

    inventory creation function working

--- 03/ft_inventory_system.py
+++ 03/ft_inventory_system.py
@@ -2,38 +2,27 @@ import sys
 
 
 def create_inventory() -> dict[str, int]:
-
     pairs = sys.argv[1:]
     inventory: dict[str, int] = {}
     for pair in pairs:
         try:
             key: str = (pair.split(':'))[0]
-            value: str = (pair.split(':'))[1]
-            inventory.update({key: int(value)})
+            if key in inventory.keys():
+                print(f"Redundant item: '{key}' - discarding")
+                continue
+            value: int = int((pair.split(':'))[1])
+            inventory.update({key: value})
         except IndexError:
-            print(f"Invalid parameter '{pair}'")
+            print(f"Error - invalid parameter '{pair}'")
         except ValueError as err2:
             print(f"Quantity error for 'key': {err2}")
-    print(inventory)
+    print(f"Got inventory: {inventory}")
     return(inventory)
-
-
-'''
-def parser() -> list[str]:
-    args: list[str] = sys.argv
-    args.remove(args[0])
-    print(args)
-    for arg in arg:
-        key, value = s.split(arg)
-
-    return(args)
-'''
 
 
 def main() -> None:
     print("=== Inventory System Analysis ===")
-    print(create_inventory())
-
+    create_inventory()
 
 if __name__ == "__main__":
     main()
