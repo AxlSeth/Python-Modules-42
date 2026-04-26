33a768cc0025544023b433b77d58032b3be64bb7
Axl <axlramaroson25@gmail.com>
2026-04-26 12:46:26 +0300

    andeha hody

--- module04/ft_ancient_text.py
+++ module04/ft_ancient_text.py
@@ -1,8 +1,24 @@
-import typing
+import sys
+
+
+def read_file() -> None:
+    file: str = sys.argv[1]
+    try:
+        print(f"Accessing file '{file}'")
+        with open(file) as f:
+            print("---\n")
+            print(f.read())
+            print("---")
+        print(f"File '{file}' closed")
+    except FileNotFoundError as err1:
+        print(f"Error opening file '{file}': {err1}") 
+    except PermissionError as err2:
+        print(f"Error opening file '{file}': {err2}")
 
 
 def main() -> None:
     print("=== Cyber Archives Recovery ===")
+    read_file()
 
 
 if __name__ == "__main__":
