6dee27c85148b3bffa23ece489d111e6ac85b62b
Axl <axlramaroson25@gmail.com>
2026-05-12 20:43:36 +0300

    Add shebang and print statement in main function
    Added a shebang line and modified the main function to include a print statement.

--- module05/data_processor.py
+++ module05/data_processor.py
@@ -1,5 +1,16 @@
+#!usr/bin/env python3.13
+
+
+from abc import ABC
+
+
+class DataProcessor(ABC):
+    def __init__(self) -> None:
+        processed_data: list[Any] = []
+
+
 def main() -> None:
-    ...
+    print("=== Garden Data Processor ===")
 
 
 if __name__ == "__main__":
