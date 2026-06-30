dd270222a855e960c394ffc6a3583724ab639a77
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-06-30 15:33:15 +0300

    finished the data processor abstract function

--- module05/data_processor.py
+++ module05/data_processor.py
@@ -1,9 +1,26 @@
-import abc
-import typing
+from abc import abstractmethod, ABC
+from typing import Any
 
 
-class DataProcessor(abc.ABC):
-    def __init__(self, data: list[int | float | list[int | float]]) -> None:
+class DataProcessor(ABC):
+    def __init__(self) -> None:
+        self.ingested_data: list[Any] = []
+        self.ingested_count: int = 0
+        self.rank: int = 0
+
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        ...
+
+    @abstractmethod
+    def ingest(self, data: Any) -> None:
+        ...
+
+    def output(self) -> tuple[int, str]:
+        outp = (self.rank, str(self.ingested_data[0]))
+        self.rank += 1
+        self.ingested_data.pop(0)
+        return outp
         
 
 
@@ -15,3 +32,13 @@ class NumericProcessor(DataProcessor):
 
 class LogProcessor(DataProcessor):
     ...
+
+
+
+
+def main() -> None:
+    print("=== Code Nexus - Data Processor ===\n")
+
+
+if __name__ == "__main__":
+    main()
