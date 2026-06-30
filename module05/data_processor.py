15b54024c142bae79a5bf24df053438b0f978c4b
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-06-30 18:09:18 +0300

    productivity

--- module05/data_processor.py
+++ module05/data_processor.py
@@ -25,10 +25,52 @@ class DataProcessor(ABC):
 
 
 class TextProcessor(DataProcessor):
-    ...
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, str):
+            return True
+        elif isinstance(data, list):
+            for element in data:
+                if not isinstance(element, str):
+                    return False
+            return True
+        else:
+            return False
+
+    def ingest(self, data: str | list[str]) -> None:
+        if self.validate(data):
+            if isinstance(data, str):
+                self.ingested_count += 1
+                self.ingested_data.append(data)
+            elif isinstance(data, list):
+                self.ingested_count += len(data)
+                for element in data:
+                    self.ingested_data.append(element)
+
 
 class NumericProcessor(DataProcessor):
-    ...
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, int):
+            return True
+        if isinstance(data, float):
+            return True
+        elif isinstance(data, list):
+            for element in data:
+                if not isinstance(element, str) and not isinstance(element, float):
+                    return False
+            return True
+        else:
+            return False
+
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        if self.validate(data):
+            if isinstance(data, int) or isinstance(data, float):
+                self.ingested_count += 1
+                self.ingested_data.append(data)
+            elif isinstance(data, list):
+                self.ingested_count += len(data)
+                for element in data:
+                    self.ingested_data.append(element)
+    
 
 class LogProcessor(DataProcessor):
     ...
